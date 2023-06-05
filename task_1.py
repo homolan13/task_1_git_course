ede22bb987b3ac6e98688b209d8535ee6e35842b
AlexFEBo <alexandre.bokhobza@gmail.com>
2023-06-05 17:57:05 +0200

    add user input

--- task_1.py
+++ task_1.py
@@ -15,3 +15,16 @@ def divide(x, y):
 # Task 1: Implement a function for Exponentiation operation, similar to the existing functions above.
 # Task 2: Create an interactive user input for the calculator, allowing the user to pick an operation and input numbers.
 
+x = int(input('what is your first number'))
+y = int(input('what is your second number'))
+
+operation = input('Write the operation to perform: add, subtract, multipy, divide')
+
+if operation == 'add':
+   print(add(x,y))
+elif operation == 'subtract':
+   print(subtract(x,y))
+elif operation == 'multiply':
+   print(multiply(x,y))
+elif operation == 'divide':
+   print(divide(x,y))
